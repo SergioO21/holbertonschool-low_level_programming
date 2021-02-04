abe3f4066f37c8d6241c49fd8377370bd8106b57
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-04 00:34:57 +0000

    Usinng puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*
+* puts: Print what you and and add a new line
+*/
+#include <stdio.h>
+
+/*
+* Starting point of the program
+*/ int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
