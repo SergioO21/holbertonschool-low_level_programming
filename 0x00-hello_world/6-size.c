eb2b80f4d89212dc8bb84717bf88aad26a7f1c5c
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-04 03:08:26 +0000

    Sizze of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,14 @@
-/**
-* sizeof: calculate size of a especific type of data
-* printf: Print what you write
-* %d: reference to a int number
-*/
-
 #include <stdio.h>
-
-/*
-* Starting point of the program
-*/ int main(void)
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an init: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long init: %lu byte(s)\n", sizeof(long int));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
