c6e361600fb84632b9a35fceeeb262f4308dc40c
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-04 02:26:39 +0000

    Sizze of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,5 @@
 	printf("Size of a long init: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
 	return (0);
 }
