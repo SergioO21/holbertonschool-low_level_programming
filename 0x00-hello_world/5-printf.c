204e251909bf25d55bd15180a412cf59b9c2d982
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-04 00:25:30 +0000

    pUsing printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*
+* printf: Print what you write
+*/
+
+#include <stdio.h>
+
+/*
+* Starting point of the program
+*/ int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
