343fce30cf39de84ddb0e9403c3b156bdb0534be
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-04 00:40:07 +0000

    Usinng puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
 * Starting point of the program
 */ int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
