c6294d70be4633136862654cbc7bf0011574c3b8
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-04 01:39:17 +0000

    Sizze of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@
 * Starting point of the program
 */ int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an init: %d byte(s)\n", sizeof(int));
-	printf("Size of a long init: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an init: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long init: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
 
 	return (0);
 }
