b0956f1212f6925aa6609831f690462d5425ac48
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-04 03:44:20 +0000

    QWrite in a different way

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+/**
+* write - is a system call that is used to write data out of a buffer.
+* Syntaxis - (descriptor output , your message , number of characters it has)
+*/
+
+#include <unistd.h>
+
+/*
+* main: Starting point of the program
+* return: Normal output
+*/ int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 64);
+	return (0);
+}
