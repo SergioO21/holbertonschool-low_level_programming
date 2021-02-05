b54626afd77acc90a67f3819895c37df17473252
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-05 02:52:33 +0000

    Print numbers ASCII

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,18 @@
 */ int main(void)
 {
 	int number = 48;
+	int i = 0;
+	char nline[] = "\n";
 
 	while (number < 58)
 	{
 		putchar(number);
 		number += 1;
+	}
+
+	for (i = 0; i < 2 ; i++)
+	{
+	putchar(nline[i]);
 	}
 
 	return (0);
