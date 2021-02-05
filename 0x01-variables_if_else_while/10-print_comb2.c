e08f2151d422d2c4916b092e76b414ad647304ac
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-05 03:33:03 +0000

    Print combinations 2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+* main: Starting point of the program
+* return (0): Standart output
+* putchar: Print a character
+*/ int main(void)
+{
+	int i;
+	int space = 32;
+	int coma = 44;
+
+	for (i = 0; i < 100; i++)
+	{
+		putchar((i/10)+'0');
+		putchar((i%10)+'0');
+		putchar(coma);
+		putchar(space);
+	}
+	return (0);
+}
