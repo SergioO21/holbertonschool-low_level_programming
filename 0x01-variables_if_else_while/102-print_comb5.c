34b6103f965b1ce921f8fd216696f8656ba77d11
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-05 18:18:11 +0000

    Print combinatios 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-/*
-* main: Starting point of the program
-* return (0): Standart output
-* putchar: Print a character
-*/ int main(void)
+/**
+* main - Starting point of the program
+* Return: Standart output
+*/
+int main(void)
 {
-	int x = '0';
+	int x = '0', z = '0', y = '0', a = '0';
 
 	while  (x <= '9')
 	{
-		int y = '0';
+		y = '0';
 
 		while  (y <= '9')
 		{
-			int z = '0';
+			z = '0';
 
 			while (z <= '9')
 			{
-				int a = '0';
+				a = '0';
 
 				while (a <= '9')
 				{
@@ -29,11 +29,8 @@
 						putchar(' ');
 						putchar(z);
 						putchar(a);
-
-						if (x == '9' && y == '8' && z == '9' && a == '9')
-						{
-							break;
-						}
+					if (x == '9' && y == '8' && z == '9' && a == '9')
+						break;
 
 						putchar(',');
 						putchar(' ');
