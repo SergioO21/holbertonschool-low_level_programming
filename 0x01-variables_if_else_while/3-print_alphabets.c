322155332fb3a71c51ed35d63592b8f4f93b7ccb
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-05 00:37:03 +0000

    Print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+* main: Starting point of the program
+* return (0): Standart output
+* putchar: Print a character
+*/ int main(void)
+{
+	int i = 0;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	char alphabetUp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	while (alphabet[i] != '\0')
+	{
+		putchar(alphabet[i]);
+		i++;
+	}
+
+	i = 0;
+
+	while (alphabetUp[i] != '\0')
+	{
+		putchar(alphabetUp[i]);
+		i++;
+	}
+
+	return (0);
+}
