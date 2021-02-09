bd161647483ea08f247c0267ca243919200893f0
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-09 16:08:09 +0000

    Library

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,13 +1,6 @@
-#include <unistd.h>
+#ifndef HOLBERTON
+#define HOLBERTON
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include "_putchar.c"
+
+#endif
