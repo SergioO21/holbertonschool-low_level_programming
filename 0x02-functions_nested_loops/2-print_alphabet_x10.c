e6c2e4e1d9121ddab1a232a400188c9d8b7a5da8
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-09 16:21:26 +0000

    Print 10 alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -18,14 +18,3 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
-
-/**
- * main - Starting point of the program
- * Return: Always 0
- */
-
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
