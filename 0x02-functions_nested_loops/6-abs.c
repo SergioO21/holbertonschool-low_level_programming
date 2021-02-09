c632855d038b8016f5b48dfcc19c22f81aa5777a
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-09 22:10:17 +0000

    Print absolutes

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * int _abs - main function
+ *
+ * Description: [int _abs] Checks for positive or negative numbers
+ * Return: Absolute number
+ */
+
+int _abs(int n)
+{
+	
+	if (n < 0)
+        {
+                n = n * -1;
+                return (n);
+        }
+
+        else
+        {
+                return (n);
+        }
+}
