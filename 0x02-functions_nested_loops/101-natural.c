84e445f3c5bfc0331bcd4f32a3f2bbebee88ec06
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-10 22:46:35 +0000

    Multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - Starting point of the program
+ * Return: Always 0
+ */
+
+int main(void)
 {
 	int n, s;
 
-	for (n = 0; n <= 1024; n ++)
+	for (n = 0; n <= 1024; n++)
 	{
 		if (n % 3 == 0 || n % 5 == 0)
 			s = s + n;
