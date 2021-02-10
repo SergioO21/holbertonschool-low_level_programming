686f7b6a6d0787ddf654abfeb0268a75901f9864
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-10 23:55:26 +0000

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of the program
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0, a = 1, b = 2, c = 0;
+
+	printf("%d, %d, ", a, b);
+
+	for (i = 0; i < 50; i++)
+	{
+		c = a + b;
+		printf("%d", c);
+		a = b;
+		b = c;
+
+		if (i != 49)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
