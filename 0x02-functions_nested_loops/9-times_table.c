4806d3eeffa4b43a312ae5d4fbbf6512b0c2aee8
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-10 03:18:35 +0000

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * times_table - main function
+ *
+ * Return: void.
+ */
+
+int main(void)
+{
+	int a, b, c;
+	
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			
+			if (b > 0 && c < 10)
+				{
+				putchar (' ');
+				}
+			
+			if (c >= 10)
+				{
+				putchar((c / 10) + '0');
+				putchar((c % 10) + '0');
+				}
+				
+			else
+				putchar(c + '0');
+			
+			if (b < 9)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+		}
+		putchar('\n');
+	}
+	return 0;
+}
