dbe5fcca7561afb04eb3e65c766e8fd9c8f911d1
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-11 22:05:20 +0000

    Print numbers from 0 - 14 X10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Print numbers from 0 - 14 ten times
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, a;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (i = 1; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
