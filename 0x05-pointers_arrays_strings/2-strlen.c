79173e43adf164fcd5ffab3782a5193c919bfc7d
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-15 21:45:27 +0000

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: String pointer
+ *
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		c++;
+		s++;
+	}
+	return (c);
+}
