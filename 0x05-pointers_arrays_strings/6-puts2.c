dda3a722e97da7164ce2ee1280e1a7d0d609951c
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-16 00:13:00 +0000

    Print odd values

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print odd values
+ * @str: String pointer
+ *
+ */
+
+void puts2(char *str)
+{
+	int c = 0;
+	
+	while (*str != '\0')
+	{
+		if (c % 2 == 0)
+		{
+			_putchar(*str);
+			str++;
+			c++;
+		}
+
+		else if (c == 9)
+		{
+			break;
+		}
+
+		else
+		{
+			str++;
+			c++;
+		}
+	}
+	_putchar('\n');
+}
