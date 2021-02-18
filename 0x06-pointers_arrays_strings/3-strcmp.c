9fb0534bd4e1016299668d46dfde73e401c26dfe
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-18 21:59:18 +0000

    Compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - Compares two strings
+ *
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Difference between two strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0, i = 0;
+
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+
+	if (s1[i] != s2[i])
+		x = s1[i] - s2[i];
+
+	return (x);
+}
