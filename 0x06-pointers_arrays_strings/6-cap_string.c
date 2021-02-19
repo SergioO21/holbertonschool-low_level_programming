08c2d5fe3c7a8cd6bb69017e761015ce02b9fad4
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-19 01:11:30 +0000

    Capitalized string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ *
+ * @s: String
+ *
+ * Return: Capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char x[] = {'.', '\n', '!', '\t', ',', ';', ' ',
+		'?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; x[j] != '\0'; j++)
+			if (s[i] == x[j])
+			{
+				i++;
+				if (s[i] >= 'a' && s[i] <= 'z')
+					s[i] = s[i] - 32;
+			}
+	}
+	return (s);
+}
