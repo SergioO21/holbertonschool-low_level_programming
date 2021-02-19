e5f21c92a4563480c0ab2b6e876b7cca1eab7955
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-19 02:25:31 +0000

    Encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char l[] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
+	char n[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for(j = 0; l[j] != '\0'; j++)
+		{
+			if (s[i] == l[j])
+			{
+				s[i] = n[j];
+			}
+		}
+	}
+	return (s);
+}
