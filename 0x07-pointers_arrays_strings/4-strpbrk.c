dbc5397dbab784982f95ce0ba2381dd456b09873
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-23 00:23:37 +0000

    _strpbrk function

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 
 /**
- * _strpbrk -
+ * _strpbrk - searches a string for any of a set of bytes.
  *
- * @s:
- * @c:
+ * @s: First string.
+ * @accept: Second string.
  *
- * Return:
+ * Return: A pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found.
  */
 
 char *_strpbrk(char *s, char *accept)
