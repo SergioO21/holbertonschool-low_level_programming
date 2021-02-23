07d52289af4698c750f5be148b81c5ac9c707ec0
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-23 00:21:16 +0000

    _strpbrk function

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk -
+ *
+ * @s:
+ * @c:
+ *
+ * Return:
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
