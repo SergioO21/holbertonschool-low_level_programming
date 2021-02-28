f95c0dc416534da1a51a89d043506ed4085ff9b2
Sergio Orejarena <sergioorueda@gmail.com>
2021-02-28 17:58:07 +0000

    Print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - Print the number of arguments.
+ *
+ * @argc: Number of arguments.
+ * @argv: Value of arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
