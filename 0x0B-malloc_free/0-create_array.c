2e803ac3a0e98cc03a4607471d4bd90af8623358
Sergio Orejarena <sergioorueda@gmail.com>
2021-03-02 21:41:02 +0000

    Create an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,6 +21,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (0);
 
+	else if (ar == 0)
+		return (0);
+	
 	else
 	{
 		for (i = 0; i < size; i++)
@@ -30,6 +33,4 @@ char *create_array(unsigned int size, char c)
 
 		return (ar);
 	}
-
-	return (0);
 }
