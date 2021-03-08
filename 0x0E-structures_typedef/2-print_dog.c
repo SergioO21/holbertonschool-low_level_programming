5c4882e5bd11c34802c2bc224f6de0512a5d79ac
Sergio Orejarena <sergioorueda@gmail.com>
2021-03-08 23:18:22 +0000

    Prints a data structure dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a data structure dog.
+ *
+ * @d: Data structure.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != 0)
+	{
+		if (d->name == '\0')
+			printf("(nil)\n");
+
+		else
+			printf("Name: %s\n", d->name);
+
+		if (d->age == '\0')
+			printf("(nil)\n");
+
+		else
+			printf("Age: %f\n", d->age);
+
+		if (d->owner == '\0')
+			printf("(nil)\n");
+
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
