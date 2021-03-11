3f2355c93fd0d12be00685f5fe54842629a6ff50
Sergio Orejarena <sergioorueda@gmail.com>
2021-03-11 03:36:01 +0000

    Simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,25 +13,37 @@
 
 int main(int argc, char **argv)
 {
+	int result, a, b;
+	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (get_op_func(argv[2]) == NULL)
+	if ((*argv[2] != '+' &&
+	     *argv[2] != '-' &&
+	     *argv[2] != '*' &&
+	     *argv[2] != '/' &&
+	     *argv[2] != '%') ||
+	     argv[2][1] != 0)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", (get_op_func(argv[2]) (atoi(argv[1]), atoi(argv[3]))));
+	result = (get_op_func(argv[2]) (a, b));
+	
+	printf("%d\n", result);
 
 	return (0);
 }
