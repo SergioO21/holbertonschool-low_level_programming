0ac71475f570fefec75cc3fc03f8fa83259afb2f
Sergio Orejarena <sergioorueda@gmail.com>
2021-03-12 01:19:27 +0000

    Library

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+typedef struct type
+{
+	char *op;
+	void (*f)(va_list);
+} type;
+
 /** Prototypes */
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
