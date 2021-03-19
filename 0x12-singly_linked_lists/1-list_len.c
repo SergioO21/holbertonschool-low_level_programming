6ed484e3a93215f4ad1b0e72553808bfc4c94cf5
Sergio Orejarena <sergioorueda@gmail.com>
2021-03-19 20:24:30 +0000

    Return length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * list_len - Returns the number of elements in a linked list.
+ *
+ * @h: List.
+ *
+ * Return: The number of elements in a linked list.
+ */
+
 size_t list_len(const list_t *h)
 {
 	const list_t *l = h;
