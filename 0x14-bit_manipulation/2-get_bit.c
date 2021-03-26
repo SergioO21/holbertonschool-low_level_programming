f588fc9fda0f45c11664a78d5520be4efc758148
Sergio Orejarena <sergioorueda@gmail.com>
2021-03-26 01:01:57 +0000

    Length of a string

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,10 +11,8 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	n = n >> index;
-
-	if (n == 0)
-		return (-1);
-
-	return (n & 1);
+	if (n >> index != 0)
+		return ((n >> index) & 1);
+	
+	return (-1);
 }
