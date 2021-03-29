7da5fa0070f72a687a0a581d47457371f9b95dd7
Sergio Orejarena <sergioorueda@gmail.com>
2021-03-29 23:52:13 +0000

    Fix conditional error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,8 +24,11 @@ int create_file(const char *filename, char *text_content)
 	if (op == EOF)
 		return (-1);
 
-	while (text_content[i] != '\0')
-		i++;
+	if (!text_content)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
 
 	wr = write(op, text_content, i);
 
