ab8b7dbc5910afe8ddca62ae1fb08b8b89c85814
Sergio Orejarena <sergioorueda@gmail.com>
2021-03-29 23:44:21 +0000

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * create_file - Creates a file.
+ *
+ * @filename: The file name.
+ * @text_content: The content of the file.
+ *
+ * Return: - (1) on success, (-1) on failure (file can not be created,
+ *           file can not be written, write fails, etc).
+ *         - If the file already exists, truncate it.
+ *         - If @filename is NULL return (-1).
+ *         - If @text_content is NULL create an empty file.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op, i;
+
+	i = 0;
+
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (op == EOF)
+		return (-1);
+
+	while (text_content[i] != '\0')
+		i++;
+
+	write(op, text_content, i);
+
+	close(op);
+
+	return (1);
+}
