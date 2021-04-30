1161289435616ae9e82eee2c104ade621aaa3b3b
Sergio Orejarena <sergioorueda@gmail.com>
2021-04-30 11:34:02 -0500

    Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ *
+ * @head: Doubly linked list.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *list;
+
+	if (head)
+	{
+		while (head)
+		{
+			list = head;
+			head = head->next;
+			free(list);
+		}
+	}
+}
