a457ce232502b559eaa55da740eeed3aa9b09f6b
Sergio Orejarena <sergioorueda@gmail.com>
2021-05-01 12:29:52 -0500

    New form

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,18 +14,13 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (head)
+	for (i = 0; i < index; i++)
 	{
-		for (i = 0; i < index; i++)
-		{
-			if (!head)
-				return (NULL);
+		if (!head)
+			return (NULL);
 
-			head = head->next;
-		}
-
-		return (head);
+		head = head->next;
 	}
 
-	return (NULL);
+	return (head);
 }
