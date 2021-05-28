3e8bd8b5ae849fde88fa8f51b02dad75117ef190
Sergio Orejarena <sergioorueda@gmail.com>
2021-05-28 17:42:03 -0500

    Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ *
+ * @ht: The hash table you want to look into.
+ * @key: The key you are looking for.
+ *
+ * Return: The value associated with the element,
+ *         or (NULL) if key couldn’t be found.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *travel = NULL;
+
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	travel = ht->array[index];
+
+	while (travel)
+	{
+		if (strcmp(travel->key, key) == 0)
+			return (travel->value);
+	}
+
+	return (NULL);
+}
