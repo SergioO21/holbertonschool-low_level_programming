e7561286fc2281beae7963bb0383fbb6d39941bc
Sergio Orejarena <sergioorueda@gmail.com>
2021-07-03 21:10:05 -0500

    Add comments

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,12 +10,15 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for idx in range(len(grid[row])):
 
+            # Checks for land at the top end of the grid.
             if row == 0 and grid[row][idx] == 1:
                 perimeter += 1
 
+            # Checks for land on the left end of the grid.
             if idx == 0 and grid[row][idx] == 1:
                 perimeter += 1
 
+            # Checks if there is land in the next cell (row) of the grid.
             if (idx + 1) < len(grid[row]):
                 if grid[row][idx] == 0 and grid[row][idx + 1] == 1:
                     perimeter += 1
@@ -23,9 +26,11 @@ def island_perimeter(grid):
                 elif grid[row][idx] == 1 and grid[row][idx + 1] == 0:
                     perimeter += 1
 
+            # Checks for land on the right end of the grid.
             elif grid[row][idx] == 1:
                 perimeter += 1
 
+            # Checks if there is land in the next cell (column) of the grid.
             if (row + 1) < len(grid):
                 if grid[row][idx] == 0 and grid[row + 1][idx] == 1:
                     perimeter += 1
@@ -33,6 +38,7 @@ def island_perimeter(grid):
                 if grid[row][idx] == 1 and grid[row + 1][idx] == 0:
                     perimeter += 1
 
+            # Checks for land on the bottom end of the grid.
             elif grid[row][idx] == 1:
                 perimeter += 1
 
